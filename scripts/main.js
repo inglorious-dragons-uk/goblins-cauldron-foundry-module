b61ebc877d444b3380ef76cb0f2b75eb137dbc4a
Pedro Francis <peggsdev@gmail.com>
2024-04-22 11:17:55 +0100

    Refactored code to use the correct socket pattern to handle events emmited from The GC Server

--- scripts/main.js
+++ scripts/main.js
@@ -2,31 +2,44 @@
 Hooks.once('ready', () => {
   // This code runs once when Foundry is ready
   console.log("This code runs once core initialization is ready and game data is available");
+
+  function handleSocketEvent({ eventType, payload }) {
+    console.log('eventType ', eventType, ' payload ', payload);
+
+    switch (eventType) {
+      case "UPDATE_CHARACTER": {
+        handleUpdateCharacterSheet(payload);
+      }
+      default:
+        throw new Error('unknown event type');
+    }
+  }
   
   // Listen for 'updateActor' events from the server
-  game.socket.on('updateActor', async (data) => {
-      // Get the actor
-      let actor = game.actors.get(data.actorId);
+  function handleUpdateCharacterSheet(payload) {
+    // // Get the actor
+    let actor = game.actors.get(payload.actorId);
 
-      // Check if the actor exists
-      if (!actor) {
-          console.log(`Actor with id ${data.actorId} does not exist.`);
-          return;
-      }
+    // Check if the actor exists
+    if (!actor) {
+      console.log(`Actor with id ${payload.actorId} does not exist.`);
+      return;
+    }
 
-      // Update the actor's name
-      await actor.update({ "name": data.newName });
+    // Update the actor's name
+    actor.update({ "name": payload.characterData.name, "system.details.age.value": payload.characterData.age});
 
-      let chatMessage = `Actor's name has been updated to ${data.newName}.`;
+    let chatMessage = `Actor's name has been updated to ${payload.characterData.name}.`;
 
-      ChatMessage.create({
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker(),
-        content: chatMessage
-      });
+    ChatMessage.create({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker(),
+      content: chatMessage
+    });
 
-      console.log(chatMessage);
+    console.log('name ', payload.characterData.name, ' actorId ', payload.actorId, ' actor ', actor);
+  }
 
-      console.log(`Actor's name has been updated to ${data.newName}.`);
-  });
+  game.socket.on('module.goblins-cauldron-foundry-module', handleSocketEvent);
+
 });
