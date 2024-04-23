fcb16ed22c0ff62ade3f757984fd7f555d8055ef
Pedro Francis <peggsdev@gmail.com>
2024-04-23 15:51:46 +0100

    Added function to send chat message

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+Hooks.once('init', () => {
+  console.log("This code runs once on initialization");
+})
+
 Hooks.once('ready', () => {
   // This code runs once when Foundry is ready
   console.log("This code runs once core initialization is ready and game data is available");
@@ -11,6 +15,11 @@ function handleSocketEvent({ eventType, payload }) {
   switch (eventType) {
     case "UPDATE_CHARACTER": {
       handleUpdateCharacterSheet(payload);
+      break
+    }
+    case "SEND_TO_CHAT": {
+      handleSendToChat(payload);
+      break
     }
     default:
       //throw new Error('unknown event type');
@@ -20,31 +29,40 @@ function handleSocketEvent({ eventType, payload }) {
 
 // Listen for 'updateActor' events from the server
 function handleUpdateCharacterSheet(payload) {
-  // // Get the actor
+  
+  // Get the actor
   const actor = [...game.actors.keys()].map((key) => {
     return game.actors.get(key)
   }).find((x) => {
     return x.name === payload.actorName
   })
 
-  console.log('actorToModify ', actor._id)
-
-  // Check if the actor exists
   if (!actor) {
     console.log(`Actor with id ${actor._id} does not exist.`);
     return;
   }
 
-  // Update the actor's name
+  // Update the actor data
   actor.update(payload.data);
 
-  let chatMessage = "Actor has been updated.";
+  console.log('Actor ', payload.data.name, ' - with ID ', actor._id, ' successfully updated!');
+}
+
+function handleSendToChat(payload) {
+  const actor = [...game.actors.keys()].map((key) => {
+    return game.actors.get(key)
+  }).find((x) => {
+    return x.name === payload.actorName
+  })
+
+  if (!actor) {
+    console.log(`Actor with id ${actor._id} does not exist.`);
+    return;
+  }
 
   ChatMessage.create({
     user: game.user._id,
     speaker: ChatMessage.getSpeaker(),
-    content: chatMessage
+    content: payload.chatMessage
   });
-
-  console.log('name ', payload.data.name, ' actorId ', actor._id);
 }
