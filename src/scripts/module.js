bce2de2f36f35d2ff522fd1c557979c5fef5f3b4
Pedro Francis <peggsdev@gmail.com>
2024-11-18 12:44:00 +0000

    Added spellCast event handling

--- src/scripts/module.js
+++ src/scripts/module.js
@@ -21,6 +21,10 @@ function handleSocketEvent({ eventType, payload }) {
       handleSendToChat(payload);
       break
     }
+    case "CAST_SPELL": {
+      handleCastSpell(payload);
+      break
+    }
     case "ROLL_DICE": {
       handleDiceRoll(payload);
       break
@@ -58,7 +62,7 @@ function handleSendToChat(payload) {
 
   ChatMessage.create({
     user: payload?.actorId,
-    speaker: ChatMessage.getSpeaker(),
+    speaker: {alias: actor?.name},
     content: payload.chatMessage
   });
 }
@@ -73,3 +77,75 @@ function handleDiceRoll(payload) {
     user: payload?.actorId
   }, 'publicroll', true)
 }
+
+function handleCastSpell(payload) {
+  const spellId = payload.body.spellItem
+  const actor = game.actors.get(payload?.actorId);
+
+  if (!actor) {
+    console.log(`Actor with id ${payload?.actorId} does not exist.`);
+    return;
+  }
+
+  castSpell(actor.items.get(spellId), actor)
+}
+
+//Whisper Chat Message with HTML
+// ChatMessage.create({
+//     whisper: [game.user.id],
+//     content:
+//       `<div class="${game.system.id} chat-card item-card">
+//           <header class="card-header flexrow">
+//           <h3 class="item-name">my test</h3>
+//           </header>
+//       </div>
+//       `,
+//   })
+
+
+//Cast Spell
+function getSpellcasting(actor) {
+  return actor.spellcasting.regular[0]
+}
+
+function escapeHtml (string) {
+  const replacements = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;',
+  }
+  return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+    return replacements[s]
+  })
+}
+
+//Example - castSpell(actor.items.get("pWJOdDbcfXQw2Y7W"), actor)
+const castSpell = async (item, actor) => {
+  const dataEmbeddedItem = `data-embedded-item="${escapeHtml(JSON.stringify(item.toObject(false)))}"`
+  const dataItemId = `data-item-id="${item.id}"`
+  const spellcasting = getSpellcasting(actor)
+  
+  item.system.location.value = spellcasting.id
+  item.isFromConsumable = true // to make it embed data
+
+  Object.defineProperty(item, 'spellcasting', {
+    value: spellcasting,
+    configurable: true,
+  })
+
+  const chatMessage = await item.toMessage(null, { create: false})
+  chatMessage.content = chatMessage.content.replace(dataItemId, `${dataItemId} ${dataEmbeddedItem}`)
+  chatMessage.flags.pf2e.casting.embeddedSpell = item.toObject()
+  chatMessage._source.flags.pf2e.casting.embeddedSpell = item.toObject()
+  return ChatMessage.create(chatMessage)
+}
+
+
+
+
+
